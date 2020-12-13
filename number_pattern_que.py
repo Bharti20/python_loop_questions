d1cf351877fa4c9a60814cb02f4f3e9337958f42
Bharti20 <bhartikumari20@navgurukul.org>
2020-12-13 11:25:09 +0800

    more logical quetions

---
+++ number_pattern_que.py
@@ -0,0 +1,13 @@
+i=1
+while i<=3:
+    print(i, end=" ")
+    i=i+1
+print()
+a=3
+b=3
+while b<28:
+    b=b+a
+    print(b,end=" ")
+    a=a+1
+    
+    
---
+++ number_pattern_que2.py
@@ -0,0 +1,6 @@
+i=1
+j=0
+while i<8:
+    j=j+i
+    print(j,end=" ")
+    i=i+1
