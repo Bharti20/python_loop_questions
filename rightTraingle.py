b4fcfcc675879d6bafa9e7b4f274ccb8366da5de
Bharti20 <bhartikumari20@navgurukul.org>
2020-12-17 00:54:35 +0800

    solve one logical quetions

---
+++ rightTraingle.py
@@ -0,0 +1,16 @@
+i=0
+while i<=5:
+    j=0
+    while j<i:
+        print("*",end=" ")
+        j=j+1
+    print()
+    i=i+1
+a=4
+while a>=0:
+    x=0
+    while x<=a:
+        print("*",end=" ")
+        x=x+1
+    print()
+    a=a-1
