8c592d66157cad87ad809e5966ebc419f0bbc82f
Bharti Kumari <72262497+Bharti20@users.noreply.github.com>
2022-01-10 12:58:41 +0530

    Update HCF.py

--- HCF.py
+++ HCF.py
@@ -1,9 +1,23 @@
-n = int(input("enter the number"))
-a = int(input("enter the number"))
-i = 2
-c=0
-while i<=a:
-    if n%i==0 and a%i==0:
-        c=i
-    i = i+1
-print("HCF",c)
+number1 = input('enter the first number.....')
+number2 = input('enter the second number....')
+try:
+    num1= int(number1)
+    num2 = int(number2)
+    if num1 > num2 :
+        i = 2
+        hcf = 0
+        while i < num1:
+            if num1%i == 0 and num2%i ==0:
+                hcf = i 
+            i = i+1
+        print(hcf)
+    else:
+        i = 2
+        hcf = 0
+        while i < num1:
+            if num1%i == 0 and num2%i ==0:
+                hcf = i 
+            i = i+1
+        print(hcf)
+except Exception as e:
+    print('Enter valid input')
